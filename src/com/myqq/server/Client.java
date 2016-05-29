9c8c264f6773a835edbbac73e32356cfea1a749e
ZhengChenYang <526789319@qq.com>
2016-05-29 12:09:31 +0800

    from zcy

--- src/com/myqq/server/Client.java
+++ src/com/myqq/server/Client.java
@@ -1,7 +1,12 @@
 package com.myqq.server;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Calendar;
+
+import com.myqq.db.ConnectDB;
 
 public class Client {
 
@@ -16,6 +21,30 @@ public class Client {
 		this.id = id;
 		this.clientThread = clientThread;
 		this.lastTime = Calendar.getInstance();
+		
+		//新建客户后 从数据库中读取滞留的信息并发送。
+		OutputStream out;
+		try {
+			out = socket.getOutputStream();
+			DataOutputStream outs = new DataOutputStream(out);
+			ConnectDB conn = ConnectDB.getInstance();
+			ChatMessage[] msgs = conn.getDelayMessage(id);
+			
+			for(int i=0;i<msgs.length;i++)
+			{
+				byte[] b = msgs[i].getMessage().getBytes("UTF-8");
+				int totalLen = 1 + 4 + b.length;
+				outs.writeByte(MessageType.SERVER_SEND_MESSAGE);
+				outs.writeInt(totalLen);
+				outs.write(b);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 	/**
